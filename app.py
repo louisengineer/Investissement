df6f8bbc132baa97d558f65f99e031a355963bf6
louisthms <louistwin78thms@gmail.com>
2025-01-07 19:26:42 +0100

    add gold

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ st.title("📊 Simulateur d'investissement basé sur un actif")
 
 # Dictionnaire des actifs avec leurs tickers
 actifs = {
+    "Or (Gold Spot USD)": "GC=F",
     "S&P 500 (ETF)": "SPY",
     "Nasdaq 100 (ETF)": "QQQ",
     "Dow Jones (ETF)": "DIA",
